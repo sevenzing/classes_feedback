2145ee409c80c6c9a4a04b7c67955a57e7d59109
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-10-06 17:28:16 +0300

    representation of models

--- surveys/models.py
+++ surveys/models.py
@@ -12,7 +12,13 @@ class Survey(models.Model):
     # set default value to current time
     pub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Survey('{self.survey_short_name}')"
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200, default='Sample question text')
 
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Question('{self.question_text}')"
