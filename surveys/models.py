64a9871e57ce95f97e66a2a105dd88769cdfe905
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-10-10 23:11:58 +0300

    add permission class for CustomUser

--- surveys/models.py
+++ surveys/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 import datetime
 import random
 
@@ -11,7 +11,7 @@ class Course(models.Model):
     year = models.CharField(max_length=4, default="BS19")
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=200, default="Nikolya")
     surname = models.CharField(max_length=200, default="Shilov")
@@ -20,23 +20,26 @@ class CustomUser(AbstractBaseUser):
     is_prof = models.BooleanField(default=False)
     is_ta = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    # can user access admin page?
     is_staff = models.BooleanField(default=False)
-
+    # designates that this user has all permissions without explicitly assigning them
+    is_superuser = models.BooleanField(default=False)
+    
     courses = models.ManyToManyField(Course)
 
     USERNAME_FIELD = 'email'
 
     objects = CustomUserManager()
     
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
+    #def has_perm(self, perm, obj=None):
+    #    "Does the user have a specific permission?"
         # Simplest possible answer: Yes, always
-        return True
+    #   return super().has_perm(perm, obj=obj)
 
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
+    #def has_module_perms(self, app_label):
+    #    "Does the user have permissions to view the app `app_label`?"
+    #    # Simplest possible answer: Yes, always
+    #    return True
 
     def __str__(self):
         return self.email.__str__()
