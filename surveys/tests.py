269cec266279689a8ce242f9f9ce55c7ae536df2
German Vechtomov <german.vechtomov@tutanota.com>
2020-10-11 09:53:26 -0700

    added basic tests for creation.

--- surveys/tests.py
+++ surveys/tests.py
@@ -5,5 +5,25 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
-from classes_feedback.surveys.models import *
+from surveys.models import *
 
+class QuestionModelTests(TestCase):
+	'''
+	Test for question object creation
+	'''
+	def createQuestion(question_text):
+		return Question.objects.create(question_text=question_text,)
+
+class SurveyModelTests(TestCase):
+	'''
+	Test for survey object creation
+	'''
+	def createSuvey(survey_short_name):
+		return Survey.objects.create(survey_short_name=survey_short_name,)
+
+class CourseeGroupModelTests(TestCase):
+	'''
+	Test for group object creation
+	'''
+	def createCourseGroup(number):
+		return CourseGroup.objects.create(number=number,)
