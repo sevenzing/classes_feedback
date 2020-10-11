71e45eedfe2729b26948abde02634f67786f3c24
German Vechtomov <german.vechtomov@tutanota.com>
2020-10-11 16:33:30 +0000

    added import for tests.

--- surveys/tests.py
+++ surveys/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from classes_feedback.surveys.models import *
+
